50cb2f156e32e25d57e60f0e2dd0aa0e07f1bbcf
Rafhael <rafhael.brum@gmail.com>
2025-05-02 11:14:09 -0300

    index wrap

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 // npx tsc src/index.ts --port 3000 --origin http://dummyjson.com
 // npx ts-node src/index.ts --clear-cache
+import { clearCache } from "./cache";
+import { startProxyServer } from "./server";
 
 const portIndex = process.argv.indexOf('--port');
 const originIndex = process.argv.indexOf('--origin');
 
 if (process.argv[2] === '--clear-cache') {
-    console.log('call clearCache()');
+    clearCache();
 }
 else if (portIndex !== -1 && originIndex !== -1) {
-    const port = process.argv[portIndex + 1];
+    const port = Number(process.argv[portIndex + 1]);
     const origin = process.argv[originIndex + 1];
 
-    console.log('call startProxyServer(port, origin)');
+    startProxyServer(port, origin);
 } else {
     console.log('Invalid parameters')
 };
